bcab1501341b50018ca0f726459a271026d405e0
Biszku <dbichu@onet.pl>
2024-02-22 18:17:36 +0100

    created spying on a method od an object

--- sum.test.js
+++ sum.test.js
@@ -45,8 +45,22 @@ const fetchPromise = require("./sum");
 // mockCallback(0);
 // mockCallback(1);
 
-test("mock implementation of a basic function", () => {
-  const mock = jest.fn((x) => 42 + x);
-  expect(mock(1)).toBe(43);
-  expect(mock).toHaveBeenCalledWith(1);
+// test("mock implementation of a basic function", () => {
+//   const mock = jest.fn((x) => 42 + x);
+//   expect(mock(1)).toBe(43);
+//   expect(mock).toHaveBeenCalledWith(1);
+// });
+
+test("spying on a method of an object", () => {
+  const video = {
+    play() {
+      return true;
+    },
+  };
+
+  const spy = jest.spyOn(video, "play");
+  video.play();
+
+  expect(spy).toHaveBeenCalled();
+  spy.mockRestore();
 });
